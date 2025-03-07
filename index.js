cc1116c8e51624c58d8dbd68dbc3b1ee3e84e65e
Sandeep Singh <snayal50@gmail.com>
2025-03-07 23:06:13 +0530

    add frontend URL

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [process.env.VERCEL_URL, process.env.LOCAL_URL],
     credentials: true,
   })
 );
